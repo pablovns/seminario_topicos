8b9b4d91fe7c72b9dd2b22545bdbe1223db4c0bc
Pablo Vinicius <viniciuspablo962@gmail.com>
2022-12-01 22:18:16 -0300

    adicionar verificação da correlação

--- main.py
+++ main.py
@@ -24,29 +24,33 @@ trad = {
 st.title('Seminário de Tópicos Especiais em Informática') 
 df = pd.read_csv("houses_to_rent_v2.csv")
 df.rename(columns=trad, inplace=True) # traduz os nomes das colunas para melhor visualização
-st.dataframe(df.head(5))
+st.dataframe(df)
+st.write(df['Cidade'].value_counts())
 
 ALVO = df['Valor do aluguel (R$)']
-# verificar as correlações entre as variáveis aqui
+colunas_drop = ['Cidade', 'Permite animais', 'Mobiliado', 'Taxa de condomínio (R$)', 'Valor do aluguel (R$)', 'IPTU (R$)', 'Seguro contra incêndio (R$)', 'Total (R$)']
+df_clean = df.drop(colunas_drop, axis=1)
+
+# verificar as correlações entre as variáveis
+for col in df_clean.columns:
+    st.markdown(f"Correlação de **{col}** com o valor do aluguel: **{df_clean[col].corr(ALVO):.4f}**")
 
 st.header("Predição utilizando Regressão Linear Múltipla")
-colunas_drop = ['Cidade', 'Permite animais', 'Mobiliado', 'Taxa de condomínio (R$)', 'Valor do aluguel (R$)', 'IPTU (R$)', 'Seguro contra incêndio (R$)', 'Total (R$)']
-opcoes = list(df.drop(colunas_drop, axis=1).columns)
-
+opcoes = list(df_clean.columns)
 container = st.container()
-all = st.checkbox('Selecionar todos')
+todos = st.checkbox('Selecionar todos')
 str_input = 'Escolha o(s) parâmetro(s) de entrada para a predição: '
-if all:
+if todos:
     selecao = container.multiselect(str_input, options=opcoes, default=opcoes)
     # caso o usuário marque a opção, cria um multiselect com as opções já selecionadas (parâmetro 'default')
 else:
     selecao = container.multiselect(str_input, options=opcoes)
     # caso contrário, cria um multiselect sem nada selecionado
-
+  
 # regressão linear múltipla
 rl = LinearRegression()
-indep = df[selecao].values.reshape(-1, len(selecao)) #variável independente
-dep = ALVO.values.flatten()
+indep = df[selecao].values.reshape(-1, len(selecao)) # variável independente
+dep = ALVO.values.flatten() # variável dependente
 rl.fit(indep, dep)
 
 if selecao:
@@ -55,4 +59,4 @@ if selecao:
 
     x_arr = np.array(x).reshape(-1, len(selecao)) # converte para o formato adequado
     y_pred = rl.predict(x_arr) # calcula a predição
-    st.write(f"Preço estimado do aluguel: R$ {float(y_pred):.2f}")
+    st.markdown(f"#### Preço estimado do aluguel: R$ {float(y_pred):.2f}")
